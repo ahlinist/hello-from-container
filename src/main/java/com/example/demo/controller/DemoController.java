de98db445b130d408b952e4b2a0cd232fbdf6594
Anton Hlinisty <anton_hlinisty@epbyminw6012.minsk.epam.com>
2019-10-29 10:45:56 +0300

    v11

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -8,7 +8,7 @@ public class DemoController {
 
     @GetMapping
     public String hello() {
-        return "Hello from container! v10";
+        return "Hello from container! v11";
     }
 
     @GetMapping("/turn-off")
