167a67d1eace4f2ca9b8f04d7c1c7e51a01f93b5
Anton Hlinisty <anton_hlinisty@epbyminw6012.minsk.epam.com>
2019-11-06 12:39:34 +0300

    Add crash endpoint

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,12 +9,18 @@ public class DemoController {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello from container! v16";
+        return "Hello from container! v17";
     }
 
     @GetMapping("/hello")
     public String helloPath() {
-        return "Hello from container with context path! v16";
+        return "Hello from container with context path! v17";
+    }
+
+    @GetMapping("/crash")
+    public ResponseEntity<String> crash() {
+        return ResponseEntity.status(500)
+                .body("<h1 style=\"color: red; text-align: center;\">ERROR!</h1>");
     }
 
     @GetMapping("/turn-off")
